3239e041d277fdec49ae5fb073f685c0ea429b0a
nildomartinsjr <nildomartinsjr@gmail.com>
2025-06-13 19:22:53 +0100

    testes Cypress 1

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,6 +10,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 
+  //Exercício
   it('preenche os campos obrigatórios e envia o formulário', ()=> {
     cy.get('#firstName')
       .type('Nildo')
@@ -25,7 +26,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('be.visible')
   })
 
-  it('preenche os campos obrigatórios e envia o formulário', ()=> {
+//Exercício extra 1
+  it('preenche os campos obrigatórios e envia o formulário', ()=> { 
     cy.get('#firstName')
       .type('Nildo')
     cy.get('#lastName')
@@ -39,8 +41,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success')
       .should('be.visible')
   })
-
-  it.only('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', ()=> {
+//Exercício extra 2
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', ()=> {
     cy.get('#firstName')
     .type('nildo')
     cy.get('#lastName')
@@ -55,5 +57,91 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     .should('be.visible')
 
   }) 
+//Exercício extra 3
+it('se um valor não-numérico for digitado no campo telefone, seu valor continuará vazio', ()=>{
+  cy.get('#phone')
+    .type('abcdef')
+    .should('have.value', '')
+})
+
+//Exercício extra 4
+it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', ()=>{
+  cy.get('#firstName')
+    .type('Nildo')
+  cy.get('#lastName')
+    .type('Martins')
+    cy.get('#email')
+    .type('nildomartins@gmail.com')
+    cy.get('#phone-checkbox')
+    .check()
+    cy.get('#open-text-area')
+    .type('Obrigado pela atenção.')
+    cy.get('.button')
+    .click()
+    cy.get(".error")
+    .should('be.visible')
+})
+
+//Exercício extra 5
+it('preenche e limpa os campos nome, sobrenome, email e telefone', ()=>{
+  cy.get('#firstName')
+  .type('Nildo')
+  .should('have.value', 'Nildo')
+  .clear()
+  .should('have.value','')
+  cy.get('#lastName')
+  .type('Martins')
+  .should('have.value', 'Martins')
+  .clear()
+  .should('have.value', '')
+  cy.get('#email')
+  .type('nildomartins@gmail.com')
+  .should('have.value', 'nildomartins@gmail.com')
+  .clear()
+  .should('have.value', '')
+  cy.get('#phone')
+  .type('999888777')
+  .should('have.value', '999888777')
+  .clear()
+  .should('have.value', '')
+
+})
+
+//Exercício extra 6
+it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', ()=>{
+  cy.get('.button')
+    .click()
+  cy.get(".error")
+    .should('be.visible')
+})
+
+//Exercício extra 7 - Comando customizado que não recebe nenhum argumento
+it('envia o formuário com sucesso usando um comando customizado', ()=>{
+  cy.fillMandatoryFieldsAndSubmit()
+})
+
+//Comando customizado que recebe um objeto como argumento
+it('envia o formuário com sucesso usando um comando customizado', ()=>{
   
+  const data = {
+    firstName: 'Nildo',
+    lastName: 'Martins',
+    email: 'nildomartins@gmail.com',
+    'open-text-area': 'Obrigado'
+  }
+  cy.fillMandatoryFieldsAndSubmit1(data)
+})
+
+//Comando que recebe um objeto como argumento, com valores padrão
+it.only('envia o formuário com sucesso usando um comando customizado', ()=>{
+
+  cy.fillMandatoryFieldsAndSubmit2({
+    firstName: 'Nildo',
+    lastName: 'Martins',
+    email: 'nildomartins@gmail.com',
+    message: 'Obrigado'
+})
+    
+})
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,39 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
 
-    firstName:'Nildo',
-    lastName:'Júnior',
-    email: 'nildojr@gmail.com',
-    text:'test'
+//Comando customizado que não recebe nenhum argumento
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
 
-}) => {
+    cy.get('#firstName').type('Nildo')
+    cy.get('#lastName').type('Martins')
+    cy.get('#email').type('nildomartinsjr@gmail.com')  
+    cy.get('#open-text-area').type('Agradeço à atenção!')
+  
+})
+
+//Comando customizado que recebe um objeto como argumento
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit1', (data) =>{
+
     cy.get('#firstName').type(data.firstName)
     cy.get('#lastName').type(data.lastName)
     cy.get('#email').type(data.email)
-    cy.get('#open-text-area').type(data.text)
-    cy.contains('button', 'Enviar').click() 
+    cy.get('#open-text-area').type(data['open-text-area'])
+})
+
+//Comando que recebe um objeto como argumento, com valores padrão
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit2', (data = {}) =>{
+
+ const defaultData = {
+    firstName: 'Nildo',
+    lastName: 'Martins',
+    email: 'nildomartins@gmail.com',
+    message: 'Obrigado'
+}
+
+  const formData = {...defaultData, ...data}
+    cy.get('#firstName').type(formData.firstName)
+    cy.get('#lastName').type(formData.lastName)
+    cy.get('#email').type(formData.email)
+    cy.get('#open-text-area').type(formData.message)
+
+
 })
